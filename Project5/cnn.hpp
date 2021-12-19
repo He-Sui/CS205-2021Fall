7f00703476b87d7b157043c3a7a08641ff8d92be
sui_h <78638687+He-Sui@users.noreply.github.com>
2021-12-19 10:57:35 +0800

    update

---
+++ Project5/cnn.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include "matrix.hpp"
+#ifdef WITH_Open_BLAS
+#include <cblas.h>
+#endif
+#include <opencv4/opencv2/opencv.hpp>
+
+typedef struct conv_param
+{
+    int pad;
+    int stride;
+    int kernel_size;
+    int in_channels;
+    int out_channels;
+    float *p_weight;
+    float *p_bias;
+} conv_param;
+
+typedef struct fc_param
+{
+    int in_features;
+    int out_features;
+    float *p_weight;
+    float *p_bias;
+} fc_param;
+
+void faceDetect(string &path);
+Matrix<float> readImag(string &fileName);
+Matrix<float> ConvBNRelu(Matrix<float> &matrix, conv_param &param);
+Matrix<float> ConvBNRelu_BruteForce(Matrix<float> &matrix, conv_param &param);
+Matrix<float> MaxPool(Matrix<float> &input);
+Matrix<float> full_connected(Matrix<float> &input, fc_param &param);
+Matrix<float> im2col(Matrix<float> &im, int pad, int stride, int kernel_size, int in_channels);
