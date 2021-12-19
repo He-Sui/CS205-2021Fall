dc5ad695a47e2f98097ec9e87f24608b917ebbc1
sui_h <78638687+He-Sui@users.noreply.github.com>
2021-12-19 10:58:48 +0800

    Update main.cpp

--- Project5/main.cpp
+++ Project5/main.cpp
@@ -1,11 +1,28 @@
-
-#include "CNN.hpp"
+#include "cnn.hpp"
 #include "matrix.hpp"
 
 int main()
 {
+    cout << "Please input the path of the image: ";
     string path;
-    cin >> path;
-    faceDetect(path);
+    char c;
+    while (true)
+    {
+        cin >> path;
+        try
+        {
+            faceDetect(path);
+        }
+        catch (cv::Exception)
+        {
+            cout << "Invalid image path!" << endl;
+        }
+        cout << "Enter 'y' to detect another image, 'q' to exit!";
+        cin >> c;
+        if (c != 'y')
+            break;
+        cout << endl;
+        cout << "Please input the path of the image: ";
+    }
     return 0;
 }
